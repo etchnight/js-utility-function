84b45a31ce9136c8aebd34eb0af2f28c1a9d857b
dualwind <dualwind@qq.com>
2024-03-11 00:29:47 +0800

    feat: treeTools创建类时需提供列表或树结构；新建dev类，用于调试
    BREAKING CHANGE: treeTools创建类时需提供列表或树结构，以与config在新建类实例时指定相匹配

---
+++ src/other.ts
@@ -0,0 +1,29 @@
+export class Dev {
+  private isDev = true;
+  //private thisBind;
+  constructor(isDev: boolean) {
+    this.isDev = isDev;
+    //this.thisBind = thisBind;
+    //this.log = this.log.bind(thisBind);
+    //this.devMap = this.devMap.bind(thisBind);
+  }
+  public log = (name: string, obj: any) => {
+    if (this.isDev) {
+      console.log(name, obj);
+    }
+  };
+  public async devMap<U>(
+    list: Array<any>,
+    cb: (value: any, index?: number, array?: any[]) => U
+  ) {
+    let result: U[] = [];
+    if (this.isDev) {
+      for (let item of list) {
+        result.push(await cb(item));
+      }
+    } else {
+      result = await Promise.all(list.map(cb));
+    }
+    return result;
+  }
+}
