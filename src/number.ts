878ffb8fc962db3973b546c123935133226bae52
dualwind <dualwind@qq.com>
2024-03-17 21:39:31 +0800

    feat(number): 修改numtoChinese，增加chineseToNum

--- src/number.ts
+++ src/number.ts
@@ -1,45 +1,111 @@
 "use strict";
 
 /**
- * @description - 将数字转换成中文大写的表示，处理到万级别
- * - 例如 toChineseNum(12345)，返回 一万二千三百四十五。
+ * @description  将数字（整数）转为汉字，从零到一亿亿
+ * https://juejin.cn/post/6892372242143903758
  */
-export function toChineseNum(num: number) {
-  const getWan = (temp: number) => {
-    const changeNum = [
-      "零",
-      "一",
-      "二",
-      "三",
-      "四",
-      "五",
-      "六",
-      "七",
-      "八",
-      "九",
-    ];
-    const unit = ["", "十", "百", "千", "万"];
-    let strArr = temp.toString().split("").reverse();
-    let newNum = "";
-    for (let i = 0; i < strArr.length; i++) {
-      let nowNum = "";
-      if (i === 0 && strArr[i] === "0") {
-        nowNum = "";
-      } else if (i > 0 && strArr[i] === "0" && strArr[i - 1] === "0") {
-        nowNum = "";
-      } else {
-        nowNum =
-          changeNum[parseInt(strArr[i])] +
-          (strArr[i] === "0" ? unit[0] : unit[i]);
-      }
-      newNum = nowNum + newNum;
-    }
-    return newNum;
+export function numtoChinese(num: number) {
+  let chNums = ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"];
+  let units = [
+    "",
+    "十",
+    "百",
+    "千",
+    "万",
+    "十",
+    "百",
+    "千",
+    "亿",
+    "十",
+    "百",
+    "千",
+    "万",
+    "十",
+    "百",
+    "千",
+    "亿",
+  ]; //可继续追加更高位转换值
+  if (!num || isNaN(num)) {
+    return "零";
+  }
+  let english = num.toString().split("");
+  let result = "";
+  for (let i = 0; i < english.length; i++) {
+    let des_i = english.length - 1 - i; //倒序排列设值
+    result = units[i] + result;
+    let arr1_index = english[des_i];
+    result = chNums[parseInt(arr1_index)] + result;
+  }
+  //将【零千、零百】换成【零】 【十零】换成【十】
+  result = result.replace(/零(千|百|十)/g, "零").replace(/十零/g, "十");
+  //合并中间多个零为一个零
+  result = result.replace(/零+/g, "零");
+  //将【零亿】换成【亿】【零万】换成【万】
+  result = result.replace(/零亿/g, "亿").replace(/零万/g, "万");
+  //将【亿万】换成【亿】
+  result = result.replace(/亿万/g, "亿");
+  //移除末尾的零
+  result = result.replace(/零+$/, "");
+  //将【零一十】换成【零十】
+  //result = result.replace(/零一十/g, '零十');//貌似正规读法是零一十
+  //将【一十】换成【十】
+  result = result.replace(/^一十/g, "十");
+  return result;
+}
+
+/**
+ * https://blog.csdn.net/weixin_48888726/article/details/127774053
+ * @param chnStr
+ * @returns
+ */
+export function chineseToNum(chnStr: string) {
+  let chnNumChar = {
+    零: 0,
+    一: 1,
+    二: 2,
+    两: 2,
+    三: 3,
+    四: 4,
+    五: 5,
+    六: 6,
+    七: 7,
+    八: 8,
+    九: 9,
   };
-  let overWan = Math.floor(num / 10000);
-  let noWan = num % 10000;
-  /*if (noWan.toString().length < 4) {
-    noWan = "0" + noWan;
-  }*/
-  return overWan ? getWan(overWan) + "万" + getWan(noWan) : getWan(num);
+  let chnNameValue = {
+    十: { value: 10, secUnit: false },
+    百: { value: 100, secUnit: false },
+    千: { value: 1000, secUnit: false },
+    万: { value: 10000, secUnit: true },
+    亿: { value: 100000000, secUnit: true },
+  };
+  let rtn = 0;
+  let section = 0;
+  let number = 0;
+  let secUnit = false;
+  let str = chnStr.split("");
+
+  for (let i = 0; i < str.length; i++) {
+    let num = chnNumChar[str[i]];
+    if (typeof num !== "undefined") {
+      number = num;
+      if (i === str.length - 1) {
+        section += number;
+      }
+    } else {
+      let unit = chnNameValue[str[i]]?.value;
+      secUnit = chnNameValue[str[i]]?.secUnit;
+      if (unit) {
+        if (secUnit) {
+          section = (section + number) * unit;
+          rtn += section;
+          section = 0;
+        } else {
+          section += number * unit;
+        }
+      }
+      number = 0;
+    }
+  }
+  return rtn + section;
 }
