24f02be89dc7cf7bf6734a83bfd43c7914b89bd5
dualwind <dualwind@qq.com>
2024-03-17 22:41:10 +0800

    feat(other): promiseExample

--- src/other.ts
+++ src/other.ts
@@ -27,3 +27,56 @@ export class Dev {
     return result;
   }
 }
+
+/**
+ *
+ * @deprecated 无法实现
+ * @param fn 希望退出的函数
+ * @param throwVar 控制退出的变量名
+ * @param delay throwFn的执行间隔
+ * @returns
+ */
+export async function interruptByOut(
+  fn: () => Promise<any>,
+  throwVar: string,
+  delay = 200
+) {
+  let intervalID;
+  try {
+    intervalID = setInterval(() => {
+      const func = new Function(
+        `if (${throwVar}) {
+          console.log("退出");
+          throw "interrupt";
+        } else {
+          console.log("不退出");
+        }
+      `
+      );
+      func();
+    }, delay);
+    await fn();
+    clearInterval(intervalID);
+  } catch (e) {
+    if (e === "interrupt") {
+      clearInterval(intervalID);
+      console.log("已提前退出");
+      return;
+    }
+  }
+}
+
+/**
+ *
+ * @param timeout
+ * @param text
+ * @returns
+ */
+export const promiseExample = (timeout: number, text: string) => {
+  return new Promise<string>((resolve, reject) => {
+    setTimeout(() => {
+      console.log(text);
+      resolve(text);
+    }, timeout);
+  });
+};
